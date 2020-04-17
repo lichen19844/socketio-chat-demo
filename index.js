74b3c508909d62c619e1370213884b131d1a098d
lichen <119449234@qq.com>
2020-04-17 16:33:08 +0800

    finish websocket chat

--- index.js
+++ index.js
@@ -11,24 +11,39 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-// 当客户端和服务端建立 WebSocket 连接成功会触发 connection 事件
-io.on('connection', (socket) => {
+// webSocket回调函数
+function ioSocket(socket) {
+  // 函数执行开始
   console.log('有一个用户进来了');
-
-  socket.emit('a', 'hello lichen');
-
-  setInterval(() => {
-    socket.emit('time', Date.now())
-  }, 10000);
-
-  socket.on('hello', data => {
-    console.log(data)
-  })
-
+  // 监听退出事件
   socket.on('disconnect', () => {
-    console.log('user disconnected');
+    console.log('有一个用户离开了');
   });
-});
+
+  // 服务端给客户端发消息
+  // socket.emit('a', 'hello lichen');
+
+  // setInterval(() => {
+  //   socket.emit('time', Date.now())
+  // }, 10000);
+
+  // socket.on('hello', data => {
+  //   console.log(data)
+  //   socket.emit('hello', '你才是猪撸撸🐽+2');
+  // })
+
+  socket.on('chat-message', msg => {
+    console.log('收到客户端消息：', msg)
+    
+    // 把消息发送给当前在线的所有客户端
+    // socket.emit('') 只能发送给当前客户端
+    io.emit('chat message', msg);
+  })
+}
+
+// 当客户端和服务端建立 WebSocket 连接成功会触发 connection 事件，执行回调函数
+io.on('connection', ioSocket);
+
 
 http.listen(3000, () => {
   console.log('listening on *:3000');
